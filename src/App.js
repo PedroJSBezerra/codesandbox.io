71b3628ba0032c4888b37a8dd73171e5540deee1
Pedro Jorge Soares Bezerra <52613930+PedroJSBezerra@users.noreply.github.com>
2021-02-21 10:49:35 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import "./styles.css";
 
 export default function App() {
-  function Item(props){
+  function Item(props) {
     return (
       <div className="item">
-        <b>Name: </b>{props.name} <br/>
-        <b>Price: </b>{props.price}
+        <b>Name: </b>
+        {props.name} <br />
+        <b>Price: </b>
+        {props.price}
       </div>
-    )
+    );
   }
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <Item name="cheese" price="29.99"/>
-      <Item name="cheese" price="29.99"/>
-      <Item name="cheese" price="29.99"/>
-      <Item name="cheese" price="29.99"/>
+      <Item name="cheese" price="29.99" />
+      <Item name="coke" price="7.99" />
+      <Item name="coffee" price="2.99" />
+      <Item name="bread" price="4.55" />
     </div>
   );
 }
